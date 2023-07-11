61df402cb89ceb538924e727e30d31e62d2a49e4
duardoqueiroz <eduardoldq1@gmail.com>
2023-07-11 18:47:51 -0300

    feat: expressjs adapter

---
+++ src/infra/api/servers/express-server.ts
@@ -0,0 +1,36 @@
+import express, { Application, Response, Request } from "express";
+import HttpHandler from "../../../application/ports/http-handlers/http-handler";
+import HttpServer from "../../../application/ports/http-servers/http-server";
+
+export default class ExpressServer implements HttpServer {
+	private app: Application;
+
+	constructor() {
+		this.app = express();
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+	}
+
+	public async register(
+		method: "get" | "post",
+		path: string,
+		handler: HttpHandler
+	): Promise<void> {
+		this.app[method](path, async (req: Request, res: Response) => {
+			const request = {
+				body: req.body,
+				params: req.params,
+				query: req.query,
+				headers: req.headers,
+			};
+			const httpResponse = await handler.handle(request);
+			return res.status(httpResponse.statusCode).json(httpResponse.body);
+		});
+	}
+
+	public async start(port: number): Promise<void> {
+		this.app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	}
+}
