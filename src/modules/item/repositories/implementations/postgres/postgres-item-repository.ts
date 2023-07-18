00539efb04be24a203817a9f3eef96beb60dd36a
duardoqueiroz <eduardoldq1@gmail.com>
2023-07-18 17:13:05 -0300

    feat: item repository with postgresql sb

---
+++ src/modules/item/repositories/implementations/postgres/postgres-item-repository.ts
@@ -0,0 +1,97 @@
+import { item_category, item_status } from "@prisma/client";
+import PrismaService from "../../../../../services/prisma-service";
+import Item from "../../../entities/item";
+import IItemRepository from "../../contracts/item-repository";
+
+export default class PostgresItemRepository implements IItemRepository {
+	constructor(private readonly prisma: PrismaService) {}
+
+	public async create(item: Item): Promise<Item> {
+		const createdItem = await this.prisma.items.create({
+			data: {
+				id: item.id,
+				name: item.name,
+				description: item.description,
+				price: item.price,
+				user_id: item.userId,
+				status: item_status[item.status as keyof typeof item_status],
+				category: item_category[item.category as keyof typeof item_category],
+			},
+		});
+		return Item.buildExisting(
+			createdItem.id,
+			createdItem.user_id,
+			createdItem.name,
+			createdItem.description,
+			createdItem.price,
+			createdItem.category,
+			createdItem.status
+		);
+	}
+	public async findById(id: string): Promise<Item | undefined> {
+		const item = await this.prisma.items.findFirst({
+			where: {
+				id,
+			},
+		});
+		if (!item) {
+			return undefined;
+		}
+		return Item.buildExisting(
+			item.id,
+			item.user_id,
+			item.name,
+			item.description,
+			item.price,
+			item.category,
+			item.status
+		);
+	}
+
+	public async findAll(): Promise<Item[]> {
+		const items = await this.prisma.items.findMany();
+		return items.map((item) => {
+			return Item.buildExisting(
+				item.id,
+				item.user_id,
+				item.name,
+				item.description,
+				item.price,
+				item.category,
+				item.status
+			);
+		});
+	}
+
+	public async update(item: Item): Promise<Item | undefined> {
+		const updatedItem = await this.prisma.items.update({
+			where: {
+				id: item.id,
+			},
+			data: {
+				name: item.name,
+				description: item.description,
+				price: item.price,
+				status: item_status[item.status as keyof typeof item_status],
+				category: item_category[item.category as keyof typeof item_category],
+			},
+		});
+		return Item.buildExisting(
+			updatedItem.id,
+			updatedItem.user_id,
+			updatedItem.name,
+			updatedItem.description,
+			updatedItem.price,
+			updatedItem.category,
+			updatedItem.status
+		);
+	}
+
+	public async delete(id: string): Promise<void> {
+		await this.prisma.items.delete({
+			where: {
+				id,
+			},
+		});
+	}
+}
